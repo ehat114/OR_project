dfc263dce31127271d4d866885f88852102f90ce
AurelianoAureliano <80793406+rkam446@users.noreply.github.com>
2021-09-28 23:19:43 +1300

    solve lp

---
+++ SolveLP.py
@@ -0,0 +1,38 @@
+from numpy.lib import index_tricks
+from GenerateRoutes import Routes
+import pandas as pd
+from pulp import *
+
+
+if __name__ == "__main__":
+    fname = "WeekdaysRoutes"
+    # fname = "SaturdaysRoutes"
+
+    routes = pd.read_csv(f"{fname}.csv", index_col=0)
+
+    prob = LpProblem("Distribution Route Problem", LpMinimize)
+
+    vars = LpVariable.dicts("Route", routes.index, 0, None, LpBinary)
+
+    prob += lpSum([routes["Cost"] * vars[r] for r in routes.index]), "Total Cost"
+
+    for store in [s for s in routes.columns.tolist() if s not in ["Path", "Pallets", "Cost"]]:
+        prob += lpSum([routes[store][r] * vars[r] for r in routes.index]) == 1
+    
+    prob += lpSum([vars[r] for r in routes.index]) <= 80
+    
+    # prob.writeLP('Routes.lp')
+
+    prob.solve()
+
+    # The status of the solution is printed to the screen
+    print("Status:", LpStatus[prob.status])
+
+    # Each of the variables is printed with its resolved optimum value
+    for v in prob.variables():
+        if v.varValue == 1:
+            print(v.name, "=", v.varValue)
+
+    # The optimised objective function valof Ingredients pue is printed to the screen    
+    print("Total Cost = ", value(prob.objective))
+
