7258146d304599bbe8af2c3ebad0bb2c5ab89862
AurelianoAureliano <80793406+rkam446@users.noreply.github.com>
2021-10-02 23:02:11 +1300

    Made the alrogithm a bit more efficient

--- GenerateRoutes.py
+++ GenerateRoutes.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import os
 import re
-from CalculateAverages import getAverages
 
 
 class Routes():
-    def __init__(self, type) -> None:
+    def __init__(self, dayType) -> None:
         # Constraints
         self.maxPallets = 26
         self.maxTime = 4 * (60 ** 2)
@@ -26,31 +25,33 @@ class Routes():
         # Get the name of every store
         self.stores = [s for s in self.adj.index.tolist() if s != self.origin]
         
-        if type == "Saturdays":
+        if dayType == "Saturdays":
             matchStr = re.compile("Countdown")
             self.stores = [s for s in self.stores if matchStr.match(s)]
 
         # Initialize dataframe
         self.routes = pd.DataFrame(columns=["Path"] + ["Pallets"] + ["Cost"] + self.stores)
 
-    def getRoutes(self, type, maxStops, currentPath, time=0, pallets=0, stops=0) -> None:
-        
-        # Stopping condition
-        if stops == maxStops:
-            nextTime = time + self.adj.loc[currentPath[-1], self.origin]
+    def getRoutes(self, dayType, maxStops):
+        self.recursive(dayType, maxStops, currentPath=[self.origin])
 
-            if nextTime <= self.maxTime:
-                self.addRoute(currentPath, nextTime, pallets)
-        else:
-            for store in self.stores:
-                if store not in currentPath and (self.regions[store] == self.regions[currentPath[-1]] or len(currentPath) == 1):
-                # if store not in currentPath:
-                    demand = self.demands.loc[store, type]
-                    nextTime = time + self.adj.loc[currentPath[-1], store] + 450 * demand
-                    nextPallets = pallets + demand
+    def recursive(self, dayType, maxStops, currentPath, time=0, pallets=0, stops=0):
+        timeToOrigin = time + self.adj.loc[currentPath[-1], self.origin]
 
-                    if nextTime <= self.maxTime and nextPallets <= self.maxPallets:
-                        self.getRoutes(type, maxStops, currentPath + [store], nextTime, nextPallets, stops + 1)
+        if timeToOrigin <= self.maxTime:
+
+            if len(currentPath) != 1:
+                self.addRoute(currentPath, timeToOrigin, pallets)
+
+            if stops < maxStops:
+                for store in self.stores:
+                    if store not in currentPath and (self.regions[store] == self.regions[currentPath[-1]] or len(currentPath) == 1): 
+                        demand = self.demands.loc[store, dayType]
+                        nextTime = time + self.adj.loc[currentPath[-1], store] + 450 * demand
+                        nextPallets = pallets + demand
+
+                        if nextTime <= self.maxTime and nextPallets <= self.maxPallets:
+                            self.recursive(dayType, maxStops, currentPath + [store], nextTime, nextPallets, stops + 1)
 
 
     def addRoute(self, path, time, pallets):
@@ -61,10 +62,10 @@ class Routes():
         if os.path.exists(fname) and not overwrite:
             raise Exception(f"\"{fname}\" already exists")
         else:
-            self.routes.to_csv(f"{fname}.csv")
+            self.routes.to_csv(fname)
 
     def loadfrom(self, fname):
-        self.routes = pd.read_csv(f"{fname}.csv", index_col=0)
+        self.routes = pd.read_csv(fname, index_col=0)
 
 def getDemands():
     fname = "GeneratedFiles/AverageDemands.csv"
@@ -82,23 +83,22 @@ def getRegions():
 
 if __name__ == "__main__":
     save = 1
-    read = 1
-    type = "Weekdays"
-    # type = "Saturdays"
+    read = 0
+    dayType = "Weekdays"
+    # dayType = "Saturdays"
 
     if read:
-        routes = Routes(type)
-        routes.loadfrom(f"GeneratedFiles/{type}Routes")
+        routes = Routes(dayType)
+        routes.loadfrom(f"GeneratedFiles/{dayType}Routes.csv")
 
     else:
         # Calculate all possible routes with certain number of stops
-        routes = Routes(type)
+        routes = Routes(dayType)
 
-        for i in range(1, 3):
-            routes.getRoutes(type, i, [routes.origin])
+        routes.getRoutes(dayType, 3)
         
         if save:
-            routes.saveas(f"GeneratedFiles/{type}Routes", overwrite=True)
+            routes.saveas(f"GeneratedFiles/{dayType}Routes.csv", overwrite=True)
 
     print(f"{len(routes.routes)} routes found")
     print(routes.routes)
